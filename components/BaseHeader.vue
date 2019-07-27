035fa820f6e642609c947bd666c24dd01bf76b1f
Strift <lau.cazanove@gmail.com>
2019-07-27 19:13:42 +0200

    Update tracking

--- components/BaseHeader.vue
+++ components/BaseHeader.vue
@@ -6,9 +6,19 @@
         alt="Logo Astral"
         class="h-24"
       >
-      <button class="text-white font-title text-xl tracking-wider inline">
+      <button class="text-white font-title text-xl tracking-widest inline">
         Contact
       </button>
     </div>
   </header>
 </template>
+
+<script>
+// import { VPopover } from 'v-tooltip'
+
+// export default {
+//   components: {
+//     VPopover
+//   }
+// }
+</script>
