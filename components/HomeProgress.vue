4fcfdac695ee5add98f5cd70072f8e3c33e6c957
Strift <lau.cazanove@gmail.com>
2019-08-07 00:16:18 +0200

    Responsive progress page

--- components/HomeProgress.vue
+++ components/HomeProgress.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="flex justify-between font-sans">
-    <ProgressRaid
+  <div class="flex flex-wrap lg:justify-between font-sans px-3 xl:px-0">
+    <div
       v-for="raid in raidProgress"
       :key="raid.name"
-      :name="raid.name"
-      :image-url="raid.imageUrl"
-      :released="raid.released"
-      :bosses="raid.bosses"
-      class=""
-    />
+      class="sm:w-1/2 lg:w-64 xl:w-64 mb-12"
+    >
+      <ProgressRaid
+        :name="raid.name"
+        :image-url="raid.imageUrl"
+        :released="raid.released"
+        :bosses="raid.bosses"
+        class="mx-3 xl:mx-0"
+      />
+    </div>
   </div>
 </template>
 
