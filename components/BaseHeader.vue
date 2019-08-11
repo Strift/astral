929748d84360c979bc84143fcac65f5642f3e4b7
Strift <lau.cazanove@gmail.com>
2019-08-12 00:42:51 +0200

    Add link on logo

--- components/BaseHeader.vue
+++ components/BaseHeader.vue
@@ -1,11 +1,17 @@
 <template>
   <header class="absolute z-10 w-full p-6 xl:p-10">
     <div class="flex justify-end items-start">
-      <img
-        src="~/assets/images/astral.svg"
-        alt="Logo Astral"
-        class="h-16 lg:h-24 mr-auto"
+      <nuxt-link
+        to="/"
+        class="mr-auto"
+        title="Accueil"
       >
+        <img
+          src="~/assets/images/astral.svg"
+          alt="Logo Astral"
+          class="h-16 lg:h-24"
+        >
+      </nuxt-link>
       <!-- <BaseButton class="hidden sm:inline-flex mr-10 lg:mr-16">
         Recrutement
       </BaseButton> -->
