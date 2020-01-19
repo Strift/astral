36f33d090263ce1220142f56c8a68688cb01d9aa
Strift <lau.cazanove@gmail.com>
2020-01-19 20:24:58 +0100

    Update présentation

--- pages/index.vue
+++ pages/index.vue
@@ -10,23 +10,23 @@
         👋 Qui sommes-nous ?
       </h3>
       <p class="mb-4">
-        Astral est une <strong class="font-semibold">guilde PvE</strong> initialement formée par quatre amis jouant ensemble depuis <span class="italic">The Burning Crusade</span>, ayant ensuite été rejoints par des amis IRL et des copains de leur serveur de l'époque (Varimathras-EU &lt;3). Aujourd’hui, la guilde est une communauté forte d’une <strong class="font-semibold">cinquantaine de joueurs expérimentés</strong> du PvE sur retail ou Vanilla.
+        Astral est une <strong class="font-semibold">guilde PvE</strong> initialement formée par des amis jouant ensemble depuis <span class="italic">The Burning Crusade</span> et des copains de leur serveur de l'époque (Varimathras-EU &lt;3). Suite à un recrutement pré et post-release, la guilde est aujourd'hui une communauté forte d’une <strong class="font-semibold">cinquantaine de joueurs expérimentés</strong> du PvE sur retail ou Vanilla.
       </p>
       <h3 class="mt-8 mb-4 font-semibold">
         👪 Que pouvons nous t'apporter ?
       </h3>
       <ul class="list-disc">
         <li class="ml-5">
-          Un groupe soudé et motivé pour monter 60
+          Un clean complet du PvE HL (MC/Onyxia clean en ~1h30)
         </li>
         <li class="ml-5">
-          Du PvP Outdoor en dehors des heures de raid
+          Un raid reroll/social hebdomadaire (soft résa et random)
         </li>
         <li class="ml-5">
-          Un clean complet du PvE HL (sans viser les serveurs firsts)
+          Du PvP Outdoor/BG en dehors des heures de raid
         </li>
         <li class="ml-5">
-          Une communauté avec qui jouer à d’autres jeux (LoL, Overwatch, etc.) en attendant la release
+          Une communauté avec qui jouer à d’autres jeux (LoL, Overwatch, etc.)
         </li>
         <li class="ml-5">
           purple l00tz?
