fdab4fe0c21bc86e620be92cfdf3feb7ade9917e
Strift <lau.cazanove@gmail.com>
2020-05-15 22:16:45 +0200

    Add new article

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="attrs">
     <h1 class="font-title text-white text-4xl mb-4 border-primary border-b-4 pb-2">
-      {{ attrs.title }}
+      {{ attrs.title | removeAccents }}
     </h1>
     <div class="font-title text-blue-light mb-12">
       {{ attrs.date | localeDate | capitalize }}
@@ -23,6 +23,7 @@
 <script>
 import capitalize from '~/lib/filters/capitalize'
 import localeDate from '~/lib/filters/localeDate'
+import removeAccents from '~/lib/filters/removeAccents'
 import BaseCard from '~/components/ui/BaseCard'
 import ArticleAuthor from '~/components/blog/ArticleAuthor'
 
@@ -42,7 +43,8 @@ export default {
 
   filters: {
     capitalize,
-    localeDate
+    localeDate,
+    removeAccents
   },
 
   async created () {
@@ -70,6 +72,14 @@ export default {
     &:not(:last-child) {
       @apply mb-8;
     }
+  }
+
+  a {
+    @apply font-semibold text-primary;
+  }
+
+  code {
+    @apply bg-blue-dark bg-opacity-25 p-1 rounded border-blue-light border border-opacity-25 font-normal;
   }
 }
 </style>
